a27116a34a30d39e7b695833a2e8b3f66a453598
Matt Vegh <mdv240@gmail.com>
2020-03-31 18:55:36 -0500

    alt method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, Switch, Route } from 'react-router-dom'
 import './App.css'
+import Home from "./Home"
+import About from "./About"
 
 function App() {
 
@@ -14,8 +16,11 @@ function App() {
             </nav>
 
             <Switch>
-                <Route exact path='/' render={() => <h1>Home Page</h1>} />
-                <Route path='/about' render={() => <h1>About Page</h1>} />
+                {/* <Route exact path='/' render={() => <h1>Home Page</h1>} />
+                <Route path='/about' render={() => <h1>About Page</h1>} /> */}
+
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
             </Switch>
         </div>
     )
