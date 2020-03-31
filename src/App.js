d342e218fe536b44b774f6da40b7af600a5752dc
Matt Vegh <mdv240@gmail.com>
2020-03-31 18:41:58 -0500

    link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, {useState} from "react"
+import React from "react"
+import { Link } from 'react-router-dom'
 
 function App() {  
-    const [page, setPage] = useState("home")
-    
-    function swapPages(newPage) {
-        setPage(prevPage => newPage)
-    }
     
     return (
         <div>
-            <nav>
-                <button onClick={() => swapPages("home")}>Home</button>
-                <button onClick={() => swapPages("about")}>About</button>
-            </nav>
-            {
-                page === "home" ?
-                <h1>Home Page</h1> :
-                <h1>About page</h1>
-            }
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
         </div>
     )
 }
