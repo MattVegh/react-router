cfe67d3dc7742df0fe32c82f727841ef5a75f48b
Matt Vegh <mdv240@gmail.com>
2020-04-06 21:50:26 -0500

    forgot css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Profile from "./pages/profile/Profile"
+import './App.css'
 
 import {Switch, Route} from "react-router-dom"
 
