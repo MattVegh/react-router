1edec2acadf97643b1dab623bc2c07c42f9e6c19
Matt Vegh <mdv240@gmail.com>
2020-04-07 15:55:21 -0500

    mapped services

--- src/pages/services/ServicesList.js
+++ src/pages/services/ServicesList.js
@@ -1,9 +1,18 @@
 import React from "react"
+import serviceData from './ServicesData'
 
 function ServicesList() {
+    
+    const services = serviceData.map(service => (
+        <ul>
+            <li>{service.name}: {service.price}</li>
+            <li>{service.description}</li>
+        </ul>
+    ))
     return (
         <div>
             <h1>Services List Page</h1>
+            {services}
         </div>
     )
 }
