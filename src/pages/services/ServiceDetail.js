c89dcbb39e5d5806c9423e6e5a7539a4fc835de5
Matt Vegh <mdv240@gmail.com>
2020-04-08 22:40:49 -0500

    set up useHistory

--- src/pages/services/ServiceDetail.js
+++ src/pages/services/ServiceDetail.js
@@ -1,17 +1,24 @@
 import React from "react"
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import servicesData from './ServicesData'
 
 
 function ServiceDetail(props) {
     const { serviceId } = useParams()
+    const history = useHistory()
+    console.log(history)
     const thisService = servicesData.find(service => service._id === serviceId)
+
+    function handleClick() {
+
+    }
 
     return (
         <div>
             <h1>Service Detail Page</h1>
             <h3>{thisService.name} - ${thisService.price}</h3>
             <p>{thisService.description}</p>
+            <button onClick={handleClick}>Back to all services</button>
         </div>
     )
 }
