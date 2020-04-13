5e3b4479a79fcae215166c6c8fd0f1226aed629a
Matt Vegh <mdv240@gmail.com>
2020-04-12 19:34:51 -0500

    links and route

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Route, Link } from 'react-router-dom'
 import Home from './AppTwoComponents/Home'
 import Products from './AppTwoComponents/Products'
 
@@ -7,9 +7,17 @@ function App() {
     return (
         <div>
             <nav>
-                <a>Home</a>
-                <a>Products</a>
+                <Link to='/'>Home</Link>
+                <Link to='/products'>Products</Link>
             </nav>
+
+            <Route exact path='/'>
+                <Home />
+            </Route>
+
+            <Route path='/products'>
+                <Products />
+            </Route>
             {/*
                 Route between 2 components depending on the path:
                 1. Home page ("/")
