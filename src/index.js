e36cc889d4dcebba0a6d62aa9ee55b278587fa26
Matt Vegh <mdv240@gmail.com>
2020-04-12 19:28:52 -0500

    practice set up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppTwo from './AppTwo'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <AppTwo />
   </Router>,
   document.getElementById('root')
 );
