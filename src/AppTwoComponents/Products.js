83fbe792a6075000201fd8e548430b0868e6520a
Matt Vegh <mdv240@gmail.com>
2020-04-12 19:47:08 -0500

    working on Products component

--- src/AppTwoComponents/Products.js
+++ src/AppTwoComponents/Products.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { useParams, useHistory } from 'react-router-dom'
 import productsData from './productsData'
 
 function Products() {
 
     const products = productsData.map(product => 
-        <div>
-            <h3>{product.name} - {product.price}</h3>
+        <div key={product.id}>
+            <h3>{product.name} - ${product.price}</h3>
             <p>{product.description}</p>
         </div>)
 
@@ -14,6 +13,7 @@ console.log(products)
     return (
         <div>
             <h1>Products Page</h1>
+            {products}
         </div>
     )
 }
