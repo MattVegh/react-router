81ddfde71c0c85a473e3de52081e853eb66d38f9
Matt Vegh <mdv240@gmail.com>
2020-04-12 19:45:39 -0500

    working on products component

--- src/AppTwoComponents/Products.js
+++ src/AppTwoComponents/Products.js
@@ -4,8 +4,13 @@ import productsData from './productsData'
 
 function Products() {
 
-    
+    const products = productsData.map(product => 
+        <div>
+            <h3>{product.name} - {product.price}</h3>
+            <p>{product.description}</p>
+        </div>)
 
+console.log(products)
     return (
         <div>
             <h1>Products Page</h1>
